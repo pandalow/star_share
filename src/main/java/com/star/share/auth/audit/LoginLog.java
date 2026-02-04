f537a7e68edb45dc341fc2d31fbc0e6b635705b8
pandalow <zxj000hugh@gmail.com>
2026-02-04 18:31:39 +0000

    feat: LoginLog service, each time when login attempts, will record a LoginLog

---
+++ src/main/java/com/star/share/auth/audit/LoginLog.java
@@ -0,0 +1,22 @@
+package com.star.share.auth.audit;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginLog {
+    private Long id;
+    private Long userId;
+    private String identifier;
+    private String channel;
+    private String ip;
+    private String userAgent;
+    private String status;
+    private Instant createdAt;
+}
