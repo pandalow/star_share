c2e3b28495c9acd08816a10b81f7d483f85ed89f
pandalow <zxj000hugh@gmail.com>
2026-02-06 14:53:13 +0000

    feat: UserProfile service contains update profile and update avatar

---
+++ src/main/java/com/star/share/profile/pojo/ProfilePatchRequest.java
@@ -0,0 +1,18 @@
+package com.star.share.profile.pojo;
+
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDate;
+
+public record ProfilePatchRequest(
+        @Size(min=1, max = 50, message = "nickname must be between 1 and 50 characters") String nickname,
+        @Size(max = 512, message = "bio must be at most 512 characters") String bio,
+        @Pattern(regexp = "(?i)MALE|FEMALE|OTHER|UNKNOWN", message = "Gender must be in MALE|FEMALE|OTHER|UNKNOWN") String gender,
+        @PastOrPresent(message = "birthday must be in the past or present") LocalDate birthday,
+        @Pattern(regexp = "^[a-zA-Z0-9_]{4,32}$", message = "username must be 4-32 characters long and can only contain letters, numbers, and underscores") String zgId,
+        @Size(max = 128, message = "school name must be in 128 chars") String school,
+        String tagJson
+) {
+}
